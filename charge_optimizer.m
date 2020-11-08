db07d1214fff7ce2291faceab6e0879ef76c1d30
Rebecca McCabe <rmccabe@mit.edu>
2020-11-07 22:11:36 -0500

    working peak reduction in basic model

---
+++ charge_optimizer.m
@@ -0,0 +1,81 @@
+clear;clc;close all
+
+%% Inputs
+% Simulation parameters
+dt = .5;                        % hours
+
+% Cars to be charged
+num_cars = 3;
+capacities = [100 100 100];     % kWh
+start_soes = [10 30 20];        % percent 0-100
+end_soes = [50 80 80];          % percent 0-100
+max_charge_pwr = [100 100 100]; % kW
+start_times = [9 9 9];          % hours from midnight
+stop_times = [17 17 15];        % hours from midnight
+
+% Grid externalities
+max_grid_pwr = 200;                 % kW
+non_charging_grid_pwr = zeros(1,25);% kW - as fn of hours from midnight
+renewable_score = zeros(1,25);  % lbs CO2 / kWh
+
+%% Optimization problem definition
+time = 0:dt:24;
+num_timesteps = length(time);
+P_size = [num_timesteps, num_cars];
+P = optimvar('P', P_size, 'LowerBound',0,'UpperBound',max(max_charge_pwr));
+obj = objective(P);
+prob = optimproblem('Objective',obj);
+
+%% Optimization problem constraints
+% prevent overload on the grid
+non_charging_grid_pwr = interp1(0:24, non_charging_grid_pwr, time);
+max_grid_pwr_constr = sum(P,2) + non_charging_grid_pwr' <= max_grid_pwr;
+prob.Constraints.maxGridPwr = max_grid_pwr_constr;   
+
+% obey vehicle hardware limits
+max_car_pwr_constr = P <= repmat(max_charge_pwr,num_timesteps,1);
+prob.Constraints.maxCarPwr = max_car_pwr_constr;     
+
+% energy delivered must meet start/end soe
+energies = (end_soes - start_soes) .* capacities / 100;
+for car = 1:num_cars
+    energy_constr = trapz(time,P(:,car)) == energies(car);
+    constr_name = ['energy' num2str(car)];
+    prob.Constraints.(constr_name) = energy_constr;          
+end
+
+% power must be zero when car not plugged in
+time_mat = repmat(time', [1 num_cars]);
+idxs_not_charging = time_mat < start_times | time_mat > stop_times;
+time_constr = P(idxs_not_charging) == 0;
+prob.Constraints.time = time_constr;               
+
+%% Run optimizer
+P0 = zeros(P_size);
+x0.P = P0;
+sol = solve(prob,x0);
+
+%% Plot results
+close all
+figure; hold all
+for car=1:num_cars
+    plot(time, sol.P(:,car),'DisplayName',['Car ' num2str(car)])
+end
+xlabel('Time (hours)')
+ylabel('Power (kW)')
+legend
+improvePlot
+
+%% Helper functions
+% Optimization objective function
+function grid_variance = objective(P)
+    grid_power = sum(P,2);
+    grid_variance = variance(grid_power);
+end
+
+function v = variance(x)
+% because the matlab default function var doesn't accept optimvar inputs
+    n = length(x);
+    v = sum((x - sum(x)./n).^2) ./ (n-1); 
+end
+
